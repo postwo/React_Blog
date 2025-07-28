c5aeab6ab80f7351ddfcf1a4a5c711994eb82009
postwo <codo7717@naver.com>
2025-07-28 12:23:23 +0900

    세팅ê 기본yml 설정

---
+++ src/main/java/com/example/react_blog/ReactBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/react_blog/ReactBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
